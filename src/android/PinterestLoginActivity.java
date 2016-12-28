c9a6248114fd7d97af1743ebd4493a01b5bf57bf
Ibby <ibby93@gmail.com>
2016-12-28 02:54:55 -0500

    init

---
+++ src/android/PinterestLoginActivity.java
@@ -0,0 +1,14 @@
+package com.zyramedia.cordova.pinterest;
+
+import android.os.Bundle;
+import org.apache.cordova.*;
+
+public class PinterestLoginActivity
+{
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+
+    }
+}
