048774a620f70d55cdde6a92da1d6c8e46fa7487
Ibby <ibby93@gmail.com>
2016-12-29 01:32:53 -0500

    basic Android functionality works

--- src/android/Pinterest.java
+++ src/android/Pinterest.java
@@ -2,11 +2,12 @@ package com.zyramedia.cordova.pinterest;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.util.Log;
 
-import org.apache.cordova.CordovaActivity;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaInterface;
@@ -26,93 +27,219 @@ import java.util.List;
 
 public class Pinterest extends CordovaPlugin {
 
-    private static final String TAG = "PinterestCordovaPlugin";
-    private PDKClient pdkClient;
-    private Context context;
-    private Activity activity;
-    private String APP_ID;
+  private static final String TAG = "PinterestCordovaPlugin";
+  private PDKClient pdkClient;
+  private Context context;
+  private Activity activity;
+  private String APP_ID;
+  private String ACCESS_TOKEN;
 
-    @Override
-    public void initialize(CordovaInterface cordova, CordovaWebView webView) {
-        super.initialize(cordova, webView);
+  private final int REQUEST_CODE_LOGIN = 1000;
 
-        Log.d(TAG, "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Hello world");
-
-        this.context = webView.getContext();
-        activity = cordova.getActivity();
-
-        try {
-            ApplicationInfo applicationInfo = activity.getPackageManager().getApplicationInfo(activity.getPackageName(), PackageManager.GET_META_DATA);
-            APP_ID = applicationInfo.metaData.getString("com.zyramedia.cordova.pinterest.APP_ID");
-        } catch (PackageManager.NameNotFoundException e) {
-            Log.d(TAG, e.getMessage());
-        } catch (NullPointerException e) {
-            Log.d(TAG, e.getMessage());
-        }
-
-        pdkClient = PDKClient.configureInstance(context, "4852632033488291014");
-        PDKClient.getInstance().onConnect(context);
+  @Override
+  public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+    super.initialize(cordova, webView);
+    this.context = webView.getContext();
+    activity = cordova.getActivity();
+    try {
+      ApplicationInfo applicationInfo = activity.getPackageManager().getApplicationInfo(activity.getPackageName(), PackageManager.GET_META_DATA);
+      APP_ID = applicationInfo.metaData.getString("com.zyramedia.cordova.pinterest.APP_ID");
+      pdkClient = PDKClient.configureInstance(context, APP_ID);
+      pdkClient.onConnect(context);
+      PDKClient.setDebugMode(true);
+    } catch (PackageManager.NameNotFoundException e) {
+      Log.d(TAG, e.getMessage());
+    } catch (NullPointerException e) {
+      Log.d(TAG, e.getMessage());
     }
 
-    @Override
-    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+    cordova.setActivityResultCallback(this);
+  }
 
-        if (action.equals("login")) {
-            login(args, callbackContext);
+  @Override
+  public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+    if (requestCode == 8772) {
+      String response = intent.getStringExtra("PDKCLIENT_EXTRA_RESULT");
+      Uri parsedResponse = Uri.parse(response);
+      ACCESS_TOKEN = parsedResponse.getQueryParameter("access_token");
+      pdkClient.onOauthResponse(requestCode, resultCode, intent);
+    }
+  }
+
+  @Override
+  public void onNewIntent(Intent intent) {
+    handleIntent(intent);
+  }
+
+  private void handleIntent(Intent intent) {
+    Log.d(TAG, "New intent");
+    final String result = intent.getData().toString();
+    intent.putExtra("PDKCLIENT_EXTRA_RESULT", result);
+    pdkClient.onOauthResponse(8772, -1, intent);
+    ACCESS_TOKEN = intent.getData().getQueryParameter("access_token");
+  }
+
+  @Override
+  public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+
+    if (action.equals("login")) {
+      login(args, callbackContext);
+      return true;
+    }
+
+    String fields = null;
+
+    try {
+      if (args.length() > 0) {
+        fields = args.getString(0);
+      }
+    } catch (JSONException e) {
+      Log.d(TAG, "Error parsing args");
+      callbackContext.error(e.toString());
+      return false;
+    }
+
+    Log.i(TAG, args.toString());
+
+    if (action.equals("getMe")) {
+      Log.d(TAG, fields);
+      pdkClient.getMe(fields, getCallback(callbackContext));
+
+    } else if (action.equals("getMyPins")) {
+
+      pdkClient.getMyPins(fields, getCallback(callbackContext));
+
+    } else if (action.equals("getMyBoards")) {
+
+      pdkClient.getMyBoards(fields, getCallback(callbackContext));
+
+    } else if (action.equals("getMyLikes")) {
+
+      pdkClient.getMyLikes(fields, getCallback(callbackContext));
+
+    } else if (action.equals("getMyFollowers")) {
+
+      pdkClient.getMyFollowers(fields, getCallback(callbackContext));
+
+    } else if (action.equals("getMyFollowedBoards")) {
+
+      pdkClient.getMyFollowedBoards(fields, getCallback(callbackContext));
+
+    } else if (action.equals("getMyFollowedInterests")) {
+
+      pdkClient.getMyFollowedInterests(fields, getCallback(callbackContext));
+
+    } else if (action.equals("getUser")) {
+
+      try {
+        pdkClient.getUser(args.getString(1), fields, getCallback(callbackContext));
+      } catch (JSONException e) {
+        Log.d(TAG, "Error getting user");
+        callbackContext.error(e.getMessage());
+      }
+
+    } else if (action.equals("api")) {
+
+      callAPI(args, callbackContext);
+
+    } else {
+
+      return false;
+
+    }
+
+    return true;
+
+  }
+
+  private PDKCallback getCallback(final CallbackContext callbackContext) {
+    return new PDKCallback() {
+      @Override
+      public void onSuccess(PDKResponse response) {
+        if (response.getData() instanceof JSONArray) {
+          callbackContext.success((JSONArray) response.getData());
+        } else if(response.getData() instanceof JSONObject) {
+          callbackContext.success((JSONObject) response.getData());
         } else {
-            return false;
+          try {
+            JSONObject res = new JSONObject(response.getData().toString());
+            callbackContext.success(res);
+          } catch (JSONException e) {
+            callbackContext.error(e.getMessage());
+          }
         }
-
-        return true;
-    }
-
-    private void login(JSONArray jsonScopes, final CallbackContext callbackContext) {
-        List scopes = new ArrayList<String>();
-
-        Log.d(TAG, jsonScopes.toString());
-
+      }
+      @Override
+      public void onFailure(PDKException exception) {
+        Log.d(TAG, "CALLBACK ERROR");
+        Log.e(TAG, exception.getDetailMessage());
         try {
-            if (jsonScopes.length() > 0) {
-                Log.d(TAG, "jsonScopes exists, and there are " + jsonScopes.length() + " of them");
-                int len = jsonScopes.length();
-                for (int i=0; i<len; i++) {
-                    scopes.add(jsonScopes.get(i).toString());
+          callbackContext.error(new JSONObject(exception.getDetailMessage()));
+        } catch (JSONException e){
+          callbackContext.error(exception.getDetailMessage());
+        }
+      }
+    };
+  }
+
+  private void callAPI(JSONArray args, final CallbackContext callbackContext) {
+
+  }
+
+
+
+
+  private void login(JSONArray jsonScopes, final CallbackContext callbackContext) {
+    final List scopes = new ArrayList<String>();
+
+    try {
+      if (jsonScopes.length() > 0) {
+        Log.d(TAG, "jsonScopes exists, and there are " + jsonScopes.length() + " of them");
+        int len = jsonScopes.length();
+        for (int i=0; i<len; i++) {
+          scopes.add(jsonScopes.get(i).toString());
+        }
+      } else {
+        Log.d(TAG, "jsonScopes do not exist");
+        scopes.add(PDKClient.PDKCLIENT_PERMISSION_READ_PUBLIC);
+        scopes.add(PDKClient.PDKCLIENT_PERMISSION_WRITE_PUBLIC);
+      }
+
+      cordova.getThreadPool().execute(new Runnable() {
+        @Override
+        public void run() {
+          pdkClient.login(context, scopes, new PDKCallback() {
+            @Override
+            public void onSuccess(PDKResponse response) {
+              Log.d(TAG, response.getData().toString());
+              try {
+                JSONObject res = new JSONObject(response.getData().toString());
+
+                if (ACCESS_TOKEN != null) {
+                  res.put("access_token", ACCESS_TOKEN);
                 }
-            } else {
-                Log.d(TAG, "jsonScopes do not exist");
-                scopes.add(PDKClient.PDKCLIENT_PERMISSION_READ_PUBLIC);
-                scopes.add(PDKClient.PDKCLIENT_PERMISSION_WRITE_PUBLIC);
+
+                callbackContext.success(res);
+              } catch (JSONException e) {
+                callbackContext.error(e.getMessage());
+              }
             }
 
-            cordova.setActivityResultCallback(this);
-            cordova.getActivity().startActivityForResult();
-
-            pdkClient.login(context, scopes, new PDKCallback() {
-                @Override
-                public void onSuccess(PDKResponse response) {
-                    Log.d(TAG, response.getData().toString());
-                    try {
-                        JSONObject res = new JSONObject(response.getData().toString());
-                        callbackContext.success(res);
-                    } catch (JSONException e) {
-                        callbackContext.error(e.getMessage());
-                    }
-                    //user logged in, use response.getUser() to get PDKUser object
-                }
-
-                @Override
-                public void onFailure(PDKException exception) {
-                    Log.e(TAG, exception.getDetailMessage());
-                    callbackContext.error(exception.getDetailMessage());
-                }
-            });
-
-        } catch (JSONException e) {
-            Log.d(TAG, e.getMessage());
+            @Override
+            public void onFailure(PDKException exception) {
+              Log.e(TAG, exception.getDetailMessage());
+              callbackContext.error(exception.getDetailMessage());
+            }
+          });
         }
+      });
 
-
-
+    } catch (JSONException e) {
+      Log.d(TAG, e.getMessage());
     }
+
+
+
+  }
 
 }
--- www/Pinterest.js
+++ www/Pinterest.js
@@ -1,6 +1,6 @@
 var exec = require('cordova/exec');
 
-module.exports = {
+var Pinterest = {
 
     SCOPES: {
         READ_PUBLIC: 'read_public',
@@ -11,8 +11,57 @@ module.exports = {
         WRITE_RELATIONSHIPS: 'write_relationships'
     },
 
+    DEFAULT_FIELDS: {
+        USER: 'id,username,first_name,last_name,bio,created_at,counts,image',
+        BOARD: 'id,name,url,description,creator,created_at,counts,image',
+        PIN: 'id,link,url,creator,board,created_at,note,color,counts,media,attribution,image,metadata',
+        INTEREST: 'id,name'
+    },
+
     login: function(scopes, onSuccess, onError) {
         exec(onSuccess, onError, 'Pinterest', 'login', scopes);
+    },
+
+    getMe: function(onSuccess, onError, fields) {
+        fields = fields || Pinterest.DEFAULT_FIELDS.USER;
+        exec(onSuccess, onError, 'Pinterest', 'getMe', [fields]);
+    },
+
+    getMyPins: function(onSuccess, onError, fields) {
+        fields = fields || Pinterest.DEFAULT_FIELDS.PIN;
+        exec(onSuccess, onError, 'Pinterest', 'getMyPins', [fields]);
+    },
+
+    getMyBoards: function(onSuccess, onError, fields) {
+        fields = fields || Pinterest.DEFAULT_FIELDS.BOARD;
+        exec(onSuccess, onError, 'Pinterest', 'getMyBoards', [fields]);
+    },
+
+    getMyLikes: function(onSuccess, onError, fields) {
+        fields = fields || Pinterest.DEFAULT_FIELDS.PIN;
+        exec(onSuccess, onError, 'Pinterest', 'getMyLikes', [fields]);
+    },
+
+    getMyFollowers: function(onSuccess, onError, fields) {
+        fields = fields || Pinterest.DEFAULT_FIELDS.USER;
+        exec(onSuccess, onError, 'Pinterest', 'getMyFollowers', [fields]);
+    },
+
+    getMyFollowedBoards: function(onSuccess, onError, fields) {
+        fields = fields || Pinterest.DEFAULT_FIELDS.BOARD;
+        exec(onSuccess, onError, 'Pinterest', 'getMyFollowedBoards', [fields]);
+    },
+
+    getMyFollowedInterests: function(onSuccess, onError, fields) {
+        fields = fields || Pinterest.DEFAULT_FIELDS.INTEREST;
+        exec(onSuccess, onError, 'Pinterest', 'getMyFollowedInterests', [fields]);
+    },
+
+    getUser: function(userId, onSuccess, onError, fields) {
+        fields = fields || Pinterest.DEFAULT_FIELDS.USER;
+        exec(onSuccess, onError, 'Pinterest', 'getUser', [fields, userId]);
     }
 
 };
+
+module.exports = Pinterest;
