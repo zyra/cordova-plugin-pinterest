e7867d320ae037fbcbc16f6c519bbed66afc0cba
Ibby <ibby93@gmail.com>
2016-12-30 08:19:19 -0500

    docs(): complete documentation for JavaScript API

--- www/Pinterest.js
+++ www/Pinterest.js
@@ -5,8 +5,6 @@ var Pinterest = {
     SCOPES: {
         READ_PUBLIC: 'read_public',
         WRITE_PUBLIC: 'write_public',
-        READ_SECRET: 'read_secret',
-        WRITE_SECRET: 'write_secret',
         READ_RELATIONSHIPS: 'read_relationships',
         WRITE_RELATIONSHIPS: 'write_relationships'
     },
@@ -18,6 +16,12 @@ var Pinterest = {
         INTEREST: 'id,name'
     },
 
+    /**
+     * Login with Pinterest
+     * @param scopes {Array<string>} Permissions to request.
+     * @param onSuccess {Function} Callback function to execute on success
+     * @param onError {Function} Callback function to execute on error
+     */
     login: function(scopes, onSuccess, onError) {
         exec(onSuccess, onError, 'Pinterest', 'login', scopes);
     },
@@ -26,9 +30,9 @@ var Pinterest = {
 
     /**
      * Get the user's profile information
-     * @param onSuccess
-     * @param onError
-     * @param fields
+     * @param onSuccess {Function} Callback function to execute on success
+     * @param onError {Function} Callback function to execute on error
+     * @param fields {string} Fields to get, separated by commas. Defaults to all available fields.
      */
     getMe: function(onSuccess, onError, fields) {
         fields = fields || Pinterest.DEFAULT_FIELDS.USER;
@@ -37,9 +41,10 @@ var Pinterest = {
 
     /**
      * Get the user's pins
-     * @param onSuccess
-     * @param onError
-     * @param fields
+     * @param onSuccess {Function} Callback function to execute on success
+     * @param onError {Function} Callback function to execute on error
+     * @param fields {string} Fields to get, separated by commas. Defaults to all available fields.
+     * @param limit {number} Maximum number of items to return. Capped at 100. Defaults to 100.
      */
     getMyPins: function(onSuccess, onError, fields, limit) {
         fields = fields || Pinterest.DEFAULT_FIELDS.PIN;
@@ -49,9 +54,10 @@ var Pinterest = {
 
     /**
      * Get the user's boards
-     * @param onSuccess
-     * @param onError
-     * @param fields
+     * @param onSuccess {Function} Callback function to execute on success
+     * @param onError {Function} Callback function to execute on error
+     * @param fields {string} Fields to get, separated by commas. Defaults to all available fields.
+     * @param limit {number} Maximum number of items to return. Capped at 100. Defaults to 100.
      */
     getMyBoards: function(onSuccess, onError, fields, limit) {
         fields = fields || Pinterest.DEFAULT_FIELDS.BOARD;
@@ -61,9 +67,10 @@ var Pinterest = {
 
     /**
      * Get the user's likes
-     * @param onSuccess
-     * @param onError
-     * @param fields
+     * @param onSuccess {Function} Callback function to execute on success
+     * @param onError {Function} Callback function to execute on error
+     * @param fields {string} Fields to get, separated by commas. Defaults to all available fields.
+     * @param limit {number} Maximum number of items to return. Capped at 100. Defaults to 100.
      */
     getMyLikes: function(onSuccess, onError, fields, limit) {
         fields = fields || Pinterest.DEFAULT_FIELDS.PIN;
@@ -73,9 +80,10 @@ var Pinterest = {
 
     /**
      * Get the user's followers
-     * @param onSuccess
-     * @param onError
-     * @param fields
+     * @param onSuccess {Function} Callback function to execute on success
+     * @param onError {Function} Callback function to execute on error
+     * @param fields {string} Fields to get, separated by commas. Defaults to all available fields.
+     * @param limit {number} Maximum number of items to return. Capped at 100. Defaults to 100.
      */
     getMyFollowers: function(onSuccess, onError, fields, limit) {
         fields = fields || Pinterest.DEFAULT_FIELDS.USER;
@@ -85,9 +93,10 @@ var Pinterest = {
 
     /**
      * Get followed boards
-     * @param onSuccess
-     * @param onError
-     * @param fields
+     * @param onSuccess {Function} Callback function to execute on success
+     * @param onError {Function} Callback function to execute on error
+     * @param fields {string} Fields to get, separated by commas. Defaults to all available fields.
+     * @param limit {number} Maximum number of items to return. Capped at 100. Defaults to 100.
      */
     getMyFollowedBoards: function(onSuccess, onError, fields, limit) {
         fields = fields || Pinterest.DEFAULT_FIELDS.BOARD;
@@ -97,9 +106,10 @@ var Pinterest = {
 
     /**
      * Get followed interests
-     * @param onSuccess
-     * @param onError
-     * @param fields
+     * @param onSuccess {Function} Callback function to execute on success
+     * @param onError {Function} Callback function to execute on error
+     * @param fields {string} Fields to get, separated by commas. Defaults to all available fields.
+     * @param limit {number} Maximum number of items to return. Capped at 100. Defaults to 100.
      */
     getMyFollowedInterests: function(onSuccess, onError, fields, limit) {
         fields = fields || Pinterest.DEFAULT_FIELDS.INTEREST;
@@ -109,10 +119,10 @@ var Pinterest = {
 
     /**
      * Get a user's profile information by username
-     * @param username
-     * @param onSuccess
-     * @param onError
-     * @param fields
+     * @param username {string} User Id or username
+     * @param onSuccess {Function} Callback function to execute on success
+     * @param onError {Function} Callback function to execute on error
+     * @param fields {string} Fields to get, separated by commas. Defaults to all available fields.
      */
     getUser: function(username, onSuccess, onError, fields) {
         fields = fields || Pinterest.DEFAULT_FIELDS.USER;
@@ -123,10 +133,10 @@ var Pinterest = {
 
     /**
      * Get board by id
-     * @param boardId
-     * @param onSuccess
-     * @param onError
-     * @param fields
+     * @param boardId {string} Board ID
+     * @param onSuccess {Function} Callback function to execute on success
+     * @param onError {Function} Callback function to execute on error
+     * @param fields {string} Fields to get, separated by commas. Defaults to all available fields.
      */
     getBoard: function(boardId, onSuccess, onError, fields) {
         fields = fields || Pinterest.DEFAULT_FIELDS.BOARD;
@@ -135,10 +145,11 @@ var Pinterest = {
 
     /**
      * Get Pins for a specific board
-     * @param boardId
-     * @param onSuccess
-     * @param onError
-     * @param fields
+     * @param boardId {string} Board ID
+     * @param onSuccess {Function} Callback function to execute on success
+     * @param onError {Function} Callback function to execute on error
+     * @param fields {string} Fields to get, separated by commas. Defaults to all available fields.
+     * @param limit {number} Maximum number of items to return. Capped at 100. Defaults to 100.
      */
     getBoardPins: function(boardId, onSuccess, onError, fields, limit) {
         fields = fields || Pinterest.DEFAULT_FIELDS.PIN;
@@ -148,9 +159,9 @@ var Pinterest = {
 
     /**
      * Deletes the specified board
-     * @param boardId
-     * @param onSuccess
-     * @param onError
+     * @param boardId {string} Board ID
+     * @param onSuccess {Function} Callback function to execute on success
+     * @param onError {Function} Callback function to execute on error
      */
     deleteBoard: function(boardId, onSuccess, onError) {
         exec(onSuccess, onError, 'Pinterest', 'deleteBoard', [boardId]);
@@ -158,10 +169,10 @@ var Pinterest = {
 
     /**
      * Creates a board for the authenticated user
-     * @param name
-     * @param desc
-     * @param onSuccess
-     * @param onError
+     * @param name {string} Name of the board
+     * @param desc {string} Description of the board (optional)
+     * @param onSuccess {Function} Callback function to execute on success
+     * @param onError {Function} Callback function to execute on error
      */
     createBoard: function(name, desc, onSuccess, onError) {
         desc = desc || '';
@@ -172,10 +183,10 @@ var Pinterest = {
 
     /**
      * Get Pin by id
-     * @param pinId
-     * @param onSuccess
-     * @param onError
-     * @param fields
+     * @param pinId {string} Pin ID
+     * @param onSuccess {Function} Callback function to execute on success
+     * @param onError {Function} Callback function to execute on error
+     * @param fields {string} Fields to get, separated by commas. Defaults to all available fields.
      */
     getPin: function(pinId, onSuccess, onError, fields) {
         fields = fields || Pinterest.DEFAULT_FIELDS.PIN;
@@ -184,9 +195,9 @@ var Pinterest = {
 
     /**
      * Deletes the specified Pin
-     * @param pinId
-     * @param onSuccess
-     * @param onError
+     * @param pinId {string} Pin ID
+     * @param onSuccess {Function} Callback function to execute on success
+     * @param onError {Function} Callback function to execute on error
      */
     deletePin: function(pinId, onSuccess, onError) {
         exec(onSuccess, onError, 'Pinterest', 'deletePin', [pinId]);
@@ -194,12 +205,12 @@ var Pinterest = {
 
     /**
      * Creates a board for the authenticated user
-     * @param note
-     * @param boardId
-     * @param imageUrl
-     * @param link
-     * @param onSuccess
-     * @param onError
+     * @param note {string} Description of Pin
+     * @param boardId {string} Board ID
+     * @param imageUrl {string} Image URL
+     * @param link {string} Link to share (optional)
+     * @param onSuccess {Function} Callback function to execute on success
+     * @param onError {Function} Callback function to execute on error
      */
     createPin: function(note, boardId, imageUrl, link, onSuccess, onError) {
         link = link || '';
