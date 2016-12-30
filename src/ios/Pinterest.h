43de3e5c05e20477622d5e3e38299166ee520dc8
Ibby Hadeed <ibby93@gmail.com>
2016-12-30 09:20:46 -0500

    feat(ios): full ios implementation

--- src/ios/Pinterest.h
+++ src/ios/Pinterest.h
@@ -1,10 +1,27 @@
 #import <Cordova/CDV.h>
+#import "PDKResponseObject.h"
 
 @interface Pinterest : CDVPlugin
 
 @property (nonatomic, retain) NSString* accessToken;
 
+- (void (^)(NSError*)) getError:(CDVInvokedUrlCommand*) command;
+
 - (void) login:(CDVInvokedUrlCommand*)command;
 - (void) getMe:(CDVInvokedUrlCommand*)command;
+- (void) getMyPins:(CDVInvokedUrlCommand*)command;
+- (void) getMyBoards:(CDVInvokedUrlCommand*)command;
+- (void) getMyLikes:(CDVInvokedUrlCommand*)command;
+- (void) getMyFollowers:(CDVInvokedUrlCommand*)command;
+- (void) getMyFollowedBoards:(CDVInvokedUrlCommand*)command;
+- (void) getMyFollowedInterests:(CDVInvokedUrlCommand*)command;
+- (void) getUser:(CDVInvokedUrlCommand*)command;
+- (void) getBoard:(CDVInvokedUrlCommand*)command;
+- (void) getBoardPins:(CDVInvokedUrlCommand*)command;
+- (void) deleteBoard:(CDVInvokedUrlCommand*)command;
+- (void) createBoard:(CDVInvokedUrlCommand*)command;
+- (void) getPin:(CDVInvokedUrlCommand*)command;
+- (void) deletePin:(CDVInvokedUrlCommand*)command;
+- (void) createPin:(CDVInvokedUrlCommand*)command;
 
 @end
--- src/ios/Pinterest.m
+++ src/ios/Pinterest.m
@@ -31,7 +31,6 @@
 
 @end
 
-
 @implementation Pinterest
 @synthesize accessToken;
 
@@ -39,7 +38,59 @@
 {
     NSString* appId = [[NSBundle mainBundle] objectForInfoDictionaryKey:@"PinterestAppId"];
     [PDKClient configureSharedInstanceWithAppId:appId];
-    NSLog(@"I'm alive");
+}
+
+- (void (^)(PDKResponseObject*)) getSuccess:(CDVInvokedUrlCommand*) command
+{
+    return ^(PDKResponseObject* responseObject) {
+        [self sendDictionaryToJs:[responseObject parsedJSONDictionary][@"data"] withCommand:command];
+    };
+}
+
+- (void (^)(NSError*)) getError:(CDVInvokedUrlCommand*) command
+{
+    return ^(NSError* error) {
+        CDVPluginResult* result = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:error.localizedDescription];
+        [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
+    };
+}
+
+- (void)sendObjectToJs:(NSObject *)object
+           withCommand:(CDVInvokedUrlCommand *)command
+{
+    [self sendDictionaryToJs:[NSMutableDictionary dictionaryWithPropertiesOfObject:object] withCommand:command];
+}
+
+- (void)sendDictionaryToJs:(NSDictionary * )data
+               withCommand:(CDVInvokedUrlCommand *) command
+{
+    CDVPluginResult* result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:data];
+    [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
+}
+
+- (void)sendArrayToJs:(NSArray *)data
+          withCommand:(CDVInvokedUrlCommand *)command
+{
+    CDVPluginResult* result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsArray:data];
+    [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
+}
+
+- (NSDictionary*)getParamsWithFields:(NSString *) fields
+{
+    NSDictionary* const params = @{
+                                   @"fields": fields
+                                   };
+    return params;
+}
+
+- (NSDictionary*)getParamsWithFields:(NSString *) fields
+                           withLimit:(NSNumber *) limit
+{
+    NSDictionary* const params = @{
+                                   @"fields": fields,
+                                   @"limit": limit
+                                   };
+    return params;
 }
 
 - (void)handleOpenURL:(NSNotification *)notification
@@ -73,29 +124,103 @@
         
         NSMutableDictionary* data = [NSMutableDictionary dictionaryWithPropertiesOfObject:user];
         
-        
         if (accessToken != nil) {
             [data setValue:accessToken forKey:@"access_token"];
         }
         
-        CDVPluginResult* result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:data];
-        [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
+        [self sendDictionaryToJs:data withCommand:command];
         
-    } andFailure:^(NSError *error) {
-        
-        NSLog(@"Failed lol");
-        
-        CDVPluginResult* result = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:error.localizedDescription];
-        [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
-        
-    }];
-    
+    } andFailure:[self getError:command]];
     
 }
 
 - (void)getMe:(CDVInvokedUrlCommand *)command
 {
-    
+    [[PDKClient sharedInstance] getPath:@"me" parameters:[self getParamsWithFields:[command.arguments objectAtIndex:0]] withSuccess:[self getSuccess:command] andFailure:[self getError:command]];
+}
+
+
+- (void)getMyPins:(CDVInvokedUrlCommand *)command
+{
+    [[PDKClient sharedInstance] getPath:@"me/pins" parameters:[self getParamsWithFields:[command.arguments objectAtIndex:0] withLimit:[command.arguments objectAtIndex:1]] withSuccess:[self getSuccess:command] andFailure:[self getError:command]];
+}
+
+- (void)getMyBoards:(CDVInvokedUrlCommand *)command
+{
+    [[PDKClient sharedInstance] getPath:@"me/boards" parameters:[self getParamsWithFields:[command.arguments objectAtIndex:0] withLimit:[command.arguments objectAtIndex:1]] withSuccess:[self getSuccess:command] andFailure:[self getError:command]];
+}
+
+- (void)getMyLikes:(CDVInvokedUrlCommand *)command
+{
+    [[PDKClient sharedInstance] getPath:@"me/likes" parameters:[self getParamsWithFields:[command.arguments objectAtIndex:0] withLimit:[command.arguments objectAtIndex:1]] withSuccess:[self getSuccess:command] andFailure:[self getError:command]];
+}
+
+- (void)getMyFollowers:(CDVInvokedUrlCommand *)command
+{
+    [[PDKClient sharedInstance] getPath:@"me/followers" parameters:[self getParamsWithFields:[command.arguments objectAtIndex:0] withLimit:[command.arguments objectAtIndex:1]] withSuccess:[self getSuccess:command] andFailure:[self getError:command]];
+}
+
+- (void)getMyFollowedBoards:(CDVInvokedUrlCommand *)command
+{
+    [[PDKClient sharedInstance] getPath:@"me/following/boards" parameters:[self getParamsWithFields:[command.arguments objectAtIndex:0] withLimit:[command.arguments objectAtIndex:1]] withSuccess:[self getSuccess:command] andFailure:[self getError:command]];
+}
+
+- (void)getMyFollowedInterests:(CDVInvokedUrlCommand *)command
+{
+    [[PDKClient sharedInstance] getPath:@"me/following/interests" parameters:[self getParamsWithFields:[command.arguments objectAtIndex:0] withLimit:[command.arguments objectAtIndex:1]] withSuccess:[self getSuccess:command] andFailure:[self getError:command]];
+}
+
+- (void)getUser:(CDVInvokedUrlCommand *)command
+{
+    NSMutableString* path = [[NSMutableString alloc] init];
+    [path appendString:@"users/"];
+    [path appendString:[command.arguments objectAtIndex:1]];
+    [[PDKClient sharedInstance] getPath:path parameters:[self getParamsWithFields:[command.arguments objectAtIndex:0]] withSuccess:[self getSuccess:command] andFailure:[self getError:command]];
+}
+
+- (void)getBoard:(CDVInvokedUrlCommand *)command
+{
+    NSMutableString* path = [[NSMutableString alloc] init];
+    [path appendString:@"boards/"];
+    [path appendString:[command.arguments objectAtIndex:1]];
+    [[PDKClient sharedInstance] getPath:path parameters:[self getParamsWithFields:[command.arguments objectAtIndex:0]] withSuccess:[self getSuccess:command] andFailure:[self getError:command]];
+}
+
+- (void)getBoardPins:(CDVInvokedUrlCommand *)command
+{
+    NSMutableString* path = [[NSMutableString alloc] init];
+    [path appendString:@"boards/"];
+    [path appendString:[command.arguments objectAtIndex:1]];
+    [path appendString:@"/pins"];
+    [[PDKClient sharedInstance] getPath:path parameters:[self getParamsWithFields:[command.arguments objectAtIndex:0] withLimit:[command.arguments objectAtIndex:1]] withSuccess:[self getSuccess:command] andFailure:[self getError:command]];
+}
+
+- (void)deleteBoard:(CDVInvokedUrlCommand *)command
+{
+    [[PDKClient sharedInstance] deleteBoard:[command.arguments objectAtIndex:0] withSuccess:[self getSuccess:command] andFailure:[self getError:command]];
+}
+
+- (void)createBoard:(CDVInvokedUrlCommand *)command
+{
+    [[PDKClient sharedInstance] createBoard:[command.arguments objectAtIndex:0] boardDescription:[command.arguments objectAtIndex:1] withSuccess:[self getSuccess:command] andFailure:[self getError:command]];
+}
+
+- (void)getPin:(CDVInvokedUrlCommand *)command
+{
+    NSMutableString* path = [[NSMutableString alloc] init];
+    [path appendString:@"pins/"];
+    [path appendString:[command.arguments objectAtIndex:1]];
+    [[PDKClient sharedInstance] getPath:path parameters:[self getParamsWithFields:[command.arguments objectAtIndex:0]] withSuccess:[self getSuccess:command] andFailure:[self getError:command]];
+}
+
+- (void)deletePin:(CDVInvokedUrlCommand *)command
+{
+    [[PDKClient sharedInstance] deletePin:[command.arguments objectAtIndex:0] withSuccess:[self getSuccess:command] andFailure:[self getError:command]];
+}
+
+- (void)createPin:(CDVInvokedUrlCommand *)command
+{
+    [[PDKClient sharedInstance] createPinWithImageURL:[command.arguments objectAtIndex:2] link:[command.arguments objectAtIndex:3] onBoard:[command.arguments objectAtIndex:1] description:[command.arguments objectAtIndex:0] withSuccess:[self getSuccess:command] andFailure:[self getError:command]];
 }
 
 @end
